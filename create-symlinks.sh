8cb769e50a182dd8e7c9b0baa526dcf7e2c80b01
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2019-02-20 11:38:08 -0500

    Add Git configuration

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -9,3 +9,7 @@ function link_config() {
 }
 
 link_config "LaunchBar/Actions" "Library/Application Support/LaunchBar"
+
+# Setup Git configuration
+link_config "git/.gitconfig" ".gitconfig"
+git config --global --replace-all core.hooksPath "${dotfiles_dir}/git/git-hooks-core"
