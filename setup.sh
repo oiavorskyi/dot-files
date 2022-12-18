6f387ccd4d900ef917c071b207848f6bbd255a6f
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2022-12-17 19:55:50 -0500

    Add ability to install additional stuff based on passed args

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ clear
 MY_DIR="$(dirname "$0")"
 export HOMEBREW_NO_ANALYTICS=1
 
-function check_base_env_configured {
+function base_env_configured {
     [ -f $MY_DIR/.base-install.completed ]
 }
 
@@ -22,9 +22,10 @@ function mark_base_env_configured {
 }
 
 function configure_base_terminal_env {
-    check_base_env_configured\
-        && echo 'base environment configuration already completed'\
-        && exit 1 
+    if base_env_configured; then
+        echo 'Base environment configuration already completed'
+        exit 1 
+    fi
 
     # Note: Homebrew needs to be set up first
     source ${MY_DIR}/scripts/common/homebrew.sh
@@ -39,6 +40,25 @@ function configure_base_terminal_env {
    
     mark_base_env_configured 
 }
+
+function configure_addons {
+    if ! base_env_configured; then
+        echo 'Cannot install without configuring base environment first'
+        echo 'Call ./setup.sh without any parameters to configure it'
+        exit 1 
+    fi
+
+    for var in "$@"
+    do
+        FILE=${MY_DIR}/scripts/common/${var}.sh
+        echo "$FILE"
+        if [ -f $FILE ]; then
+            source ${FILE}
+        else
+            echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
+        fi
+    done
+}
     
 # Install everything else
 #source ${MY_DIR}/scripts/common/editors.sh
@@ -49,9 +69,12 @@ function configure_base_terminal_env {
 #source ${MY_DIR}/scripts/common/unix.sh
 #source ${MY_DIR}/scripts/common/configuration-osx.sh
 
-#source ${MY_DIR}/scripts/common/finished.sh
 
 # Without any parameters we should only install the minimal terminal environment
 if [ $# -eq 0 ]; then
     configure_base_terminal_env
+else
+    configure_addons "$@"
 fi
+
+source ${MY_DIR}/scripts/common/finished.sh
