6edc96d0ec85a3821c09850c8f98feb4fcd9cc98
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2022-12-22 09:32:02 -0500

    Remove AirMail and Fantastical

--- scripts/common/app-store.sh
+++ scripts/common/app-store.sh
@@ -12,6 +12,4 @@ mas install 973134470  # Be Focused - Focus Timer, for Pomodoro technique
 mas install 937984704  # Amphetamine, to keep Mac from sleeping
 mas install 406056744  # Evernote, notes manager
 mas install 1481669779 # Evernote Web Clipper, integration with Safari
-mas install 918858936  # Airmail 5, mail client of choice
-mas install 975937182  # Fantastical - Calendar, more advanced calendar
 mas install 1462114288 # Grammarly: Writing App, editor for when writing is important
