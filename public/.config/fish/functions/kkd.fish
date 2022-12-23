f0f75e60b83ee053aeb07f8e77f2d5dc2d2c006d
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2022-12-23 14:43:00 -0500

    Add kkd function to dry-run kubectl apply -k with color output

---
+++ public/.config/fish/functions/kkd.fish
@@ -0,0 +1,9 @@
+function kkd --description 'Dry run kubectl apply -k with color coding of the output'
+    set src_dir $1
+    if test -n $1
+        set src_dir .
+    end
+    kubectl apply -k $src_dir --dry-run=client -o yaml | \
+        yq 'del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration")' - | \
+        bat -l yaml
+end
