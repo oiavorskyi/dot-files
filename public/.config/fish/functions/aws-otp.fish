2691c0a3466c5b0d57f41acbc1dcf126c66c8a57
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2023-03-09 09:17:33 -0500

    --amend

--- public/.config/fish/functions/aws-otp.fish
+++ public/.config/fish/functions/aws-otp.fish
@@ -1,3 +1,6 @@
+# Requires presence of environment variable AWS_ITEM that points to the
+# item in the vault from which to read the OTP. Works nicely with direnv
+# when item is different based on the project
 function aws-otp --description 'Gets AWS OTP from vault'
     op item get --otp $AWS_ITEM
 end
